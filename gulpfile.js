4223a903ec0308902917bcd290a8e6dc32b10c2c
jake.harrison <jah71aber@gmail.com>
2017-10-16 00:20:03 +0100

    formatting changes, and removed unused elements

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,46 @@
 const gulp = require('gulp'),
-jshint = require('gulp-jshint'),
-livereload = require('gulp-livereload'),
-modRewrite = require('connect-modrewrite'),
-connect = require('gulp-connect'),
-watch = require('gulp-watch'),
-openbrowser = require('gulp-open'),
-server = require('karma').Server;
+  jshint = require('gulp-jshint'),
+  livereload = require('gulp-livereload'),
+  modRewrite = require('connect-modrewrite'),
+  connect = require('gulp-connect'),
+  watch = require('gulp-watch'),
+  openbrowser = require('gulp-open'),
+  server = require('karma').Server;
 
-gulp.task('lint', function() {
-return gulp.src('web/scripts/*.js')
-.pipe(jshint.reporter('jshint-stylish'))
-.pipe(jshint.reporter('fail'))
-.pipe(gulp.dest('./dist/web/scripts/'))
-.pipe(connect.reload());
+gulp.task('lint', function () {
+  return gulp.src('web/scripts/*.js')
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+    .pipe(gulp.dest('./dist/web/scripts/'))
+    .pipe(connect.reload());
 });
 
 gulp.task('test', function (done) {
-new server({
-  configFile: __dirname + '/karma.conf.js',
-}, done).start();
+  new server({
+    configFile: __dirname + '/karma.conf.js',
+  }, done).start();
 });
 
-gulp.task('styles', function() {
-return gulp.src('web/styles/*.css')
-.pipe(gulp.dest('./dist/web/styles/'))
-.pipe(connect.reload());
+gulp.task('styles', function () {
+  return gulp.src('web/styles/*.css')
+    .pipe(gulp.dest('./dist/web/styles/'))
+    .pipe(connect.reload());
 });
 
-gulp.task('copyhtml', function() {
-return gulp.src('web/*.html')
-.pipe(gulp.dest('./dist/web/'))
-.pipe(connect.reload());
+gulp.task('copyhtml', function () {
+  return gulp.src('web/*.html')
+    .pipe(gulp.dest('./dist/web/'))
+    .pipe(connect.reload());
 });
 
-gulp.task('watch', function() {
-gulp.watch(['web/*.html'], ['copyhtml']);
-gulp.watch(['web/styles/*.css'], ['styles']);
-gulp.watch(['web/scripts/*.js'], ['lint']);
+gulp.task('watch', function () {
+  gulp.watch(['web/*.html'], ['copyhtml']);
+  gulp.watch(['web/styles/*.css'], ['styles']);
+  gulp.watch(['web/scripts/*.js'], ['lint']);
 });
 
-gulp.task('webserver', function() {
-var options = {
+gulp.task('webserver', function () {
+  var options = {
     port: 8080,
     livereload: true,
     root: 'dist/',
@@ -51,15 +51,15 @@ var options = {
         ])
       ];
     }
-};
-connect.server(options);
+  };
+  connect.server(options);
 });
 
-gulp.task('open', ['webserver'], function() {
-return gulp.src(__filename)
-.pipe(openbrowser({
-    uri: 'http://localhost:8080/web'
-}));
+gulp.task('open', ['webserver'], function () {
+  return gulp.src(__filename)
+    .pipe(openbrowser({
+      uri: 'http://localhost:8080/web'
+    }));
 });
 
-gulp.task('dev', ['copyhtml', 'styles', 'lint', 'open', 'watch', 'test'], function() {});
+gulp.task('dev', ['copyhtml', 'styles', 'lint', 'open', 'watch', 'test'], function () { });
